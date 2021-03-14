79a8b5b27ee8e3249831c08148cf954da7549534
Kari <kikidunn2002@gmail.com>
2021-03-15 09:55:25 +1100

    pepeW

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import os
 client  = discord.Client()
 #@client.event
 #async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+print('We have logged in as {0.user}'.format(client))
 
 #@client.event
 #async def on_message(message):
