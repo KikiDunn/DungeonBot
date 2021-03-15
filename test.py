bf2aac1feea64520e26b39d18ad2b5125891fe99
Kari <kikidunn2002@gmail.com>
2021-03-15 12:19:21 +1100

    afadf

--- test.py
+++ test.py
@@ -1,17 +1,19 @@
-import discord 
-import os
+#import discord 
+#import os
 
-client  = discord.Client()
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+#client  = discord.Client()
+#@client.event
+#async def on_ready():
+#    print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!hello'):
-        await message.channel.send('Hello!')
+#@client.event
+#async def on_message(message):
+#    if message.author == client.user:
+#        return
+#
+#    if message.content.startswith('!hello'):
+#        await message.channel.send('Hello!')
+#with open('key') as fp:
+#    client.run(os.getenv(fp.readline().strip()))
 with open('key') as fp:
-    client.run(os.getenv(fp.readline().strip()))
+    print(fp.readline().strip())
