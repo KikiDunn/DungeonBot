c1ea60a5c741a2c780efda3810c06ac1ae6636c1
Kari <kikidunn2002@gmail.com>
2021-03-15 12:23:21 +1100

    agsdgagd

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
-#import discord 
-#import os
+import discord 
+import os
 
-#client  = discord.Client()
-#@client.event
-#async def on_ready():
-#    print('We have logged in as {0.user}'.format(client))
+client  = discord.Client()
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
-#@client.event
-#async def on_message(message):
-#    if message.author == client.user:
-#        return
-#
-#    if message.content.startswith('!hello'):
-#        await message.channel.send('Hello!')
-#with open('key') as fp:
-#    client.run(os.getenv(fp.readline().strip()))
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
 with open('key') as fp:
-    print(fp.readline().strip())
+    token = fp.readline().strip()
+    print(token)
+    client.run(os.getenv(token))
