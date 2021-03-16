c38edcdca242e04b1c8781c0fe3a6cac4d8e3cc2
Kari <kikidunn2002@gmail.com>
2021-03-17 10:28:28 +1100

    aa

--- bot.py
+++ bot.py
@@ -1,5 +1,12 @@
-class dog:
+import discord 
+class eventHandler:
         def __init__(self):
-            self.hello = "hello"
-        def print(self):
-            print(self.hello)
+            
+        def event(self, message):
+            if message.author == client.user:
+                return
+            else:
+                if message.content.startswith('!hello'):
+                    await message.channel.send("Hello "+ message.author.mention)
+                if message.content.startswith('!kill'):
+                    quit()
--- main.py
+++ main.py
@@ -9,21 +9,14 @@ import os
 import bot
 
 client  = discord.Client()
-dog = bot.dog()
-dog.print()
+_bot = bot.eventHandler()
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-    else:
-        if message.content.startswith('!hello'):
-            await message.channel.send("Hello "+ message.author.mention)
-        if message.content.startswith('!kill'):
-            quit()
+    _bot.event(message)
 with open('key') as fp:
     token = fp.readline().strip()
     client.run(token)
